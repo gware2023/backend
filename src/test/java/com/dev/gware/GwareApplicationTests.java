fe5ab7515b39ae8ddd5aa707895a573e415e7c3f
neuma573 <crye@kakao.com>
2023-03-27 21:28:09 +0900

    first commit

---
+++ src/test/java/com/dev/gware/GwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.gware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

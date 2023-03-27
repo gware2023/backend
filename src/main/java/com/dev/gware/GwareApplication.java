03fdd8b69bf37baab08b54c992be696f0a20264e
neuma573 <crye@kakao.com>
2023-03-27 21:28:09 +0900

    first commit

---
+++ src/main/java/com/dev/gware/GwareApplication.java
@@ -0,0 +1,13 @@
+package com.dev.gware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GwareApplication.class, args);
+	}
+
+}

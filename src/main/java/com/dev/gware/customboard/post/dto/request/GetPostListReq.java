3172402758d7f3cca56b90474bcc653be2d66636
노호준 <hh950131@gmail.com>
2023-04-14 15:35:01 +0900

    검증 어노테이션 추가

--- src/main/java/com/dev/gware/customboard/post/dto/request/GetPostListReq.java
+++ src/main/java/com/dev/gware/customboard/post/dto/request/GetPostListReq.java
@@ -6,13 +6,17 @@ import lombok.ToString;
 import org.hibernate.validator.constraints.Range;
 
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
 @ToString
 public class GetPostListReq {
+    @NotNull
     @Min(1L)
     long boardId;
+
+    @NotNull
     @Range(min = 1, max = 1000)
     int pageNum;
 }

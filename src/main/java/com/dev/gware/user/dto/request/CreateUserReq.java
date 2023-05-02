b7361d0eb90d828ad129fb888cf2ae8798f82289
노호준 <hh950131@gmail.com>
2023-05-02 16:54:46 +0900

    생성자 어노테이션 추가

--- src/main/java/com/dev/gware/user/dto/request/CreateUserReq.java
+++ src/main/java/com/dev/gware/user/dto/request/CreateUserReq.java
@@ -1,10 +1,12 @@
 package com.dev.gware.user.dto.request;
 
 import com.dev.gware.user.domain.Users;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 
 @Getter
+@AllArgsConstructor
 public class CreateUserReq {
 
     private String loginId;
